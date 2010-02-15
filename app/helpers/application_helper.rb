60c47bfc4a6f66cd6f47ece7b4cff44793fd28f7
Roy Pardee <pardee.r@ghc.org>
2010-02-15 11:11:37 -0800

    Added DatasetImplementations model stuff--seems to work.  No UI yet.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  Date::DATE_FORMATS[:month_and_year] = "%b %Y"  
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -3,4 +3,7 @@ class Site < ActiveRecord::Base
   validates_uniqueness_of :name, :abbreviation, :standard_code
   has_many :dataset_implementations
   has_many "datasets", :through => :dataset_implementations
+  def location
+    "#{self.city}, #{self.state}"
+  end
 end
