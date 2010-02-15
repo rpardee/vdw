3ffb25f8dbda5444ae3eb826a08bfc2beca131f7
Roy Pardee <pardee.r@ghc.org>
2010-02-15 11:57:47 -0800

    Implementation polishing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # FIXME: Is there a convention for where these things should go?
   Date::DATE_FORMATS[:month_and_year] = "%b %Y"  
 end
--- app/models/dataset_implementation.rb
+++ app/models/dataset_implementation.rb
@@ -2,4 +2,7 @@ class DatasetImplementation < ActiveRecord::Base
   belongs_to :site
   belongs_to :dataset
   validates_presence_of :site_id, :dataset_id, :start_date, :end_date
+  def self.get_update_frequencies
+    %w(monthly annually)
+  end
 end
