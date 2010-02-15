66915a0d98fb28e2693c4a3401e9952ade930675
Roy Pardee <pardee.r@ghc.org>
2010-02-15 09:59:43 -0800

    Did a git clean.

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,6 @@
 class Site < ActiveRecord::Base
+  validates_presence_of :name, :abbreviation, :standard_code
+  validates_uniqueness_of :name, :abbreviation, :standard_code
+  has_many :dataset_implementations
+  has_many "datasets", :through => :dataset_implementations
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :dataset_implementations
+
   map.resources :sites
 
   map.resources :datasets
