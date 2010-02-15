b4811ab1fa22d9f35278e4df754908b4dcb71ba3
Roy Pardee <pardee.r@ghc.org>
2010-02-15 14:06:04 -0800

    Small tweaks--getting ready to do data entry on heroku.

--- app/models/dataset_implementation.rb
+++ app/models/dataset_implementation.rb
@@ -3,6 +3,6 @@ class DatasetImplementation < ActiveRecord::Base
   belongs_to :dataset
   validates_presence_of :site_id, :dataset_id, :start_date, :end_date
   def self.get_update_frequencies
-    %w(monthly annually)
+    %w(monthly quarterly annually)
   end
 end
