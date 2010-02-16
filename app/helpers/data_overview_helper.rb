7b48fc033ed1058a5aa13d8d29a40e82c3a7d91b
Roy Pardee <pardee.r@ghc.org>
2010-02-16 10:37:12 -0800

    Added tooltip caveats/issues to overview page.

--- app/helpers/data_overview_helper.rb
+++ app/helpers/data_overview_helper.rb
@@ -1,8 +1,9 @@
 module DataOverviewHelper
   def cell_contents(d, s)
     if s.datasets.include?(d) then 
+      # TODO: Change title to show number of current issues and caveats.
       di = s.dataset_implementations.find_by_dataset_id(d)
-      link_to "Strt: #{di.start_date.to_s(:month_and_year)} <br />Upd: #{di.update_frequency}", di, {:title => 'Click for details.'}
+      link_to "Strt: #{di.start_date.to_s(:month_and_year)} <br />Upd: #{di.update_frequency}", di, {:title => "#{di.open_issues.length} open issues; #{di.caveats.length} caveats."}
     else
       link_to "NI", {:action => "new", :controller => "dataset_implementations", :site_id => s.id, :dataset_id => d.id}, {:title => 'Click to create'}
     end
