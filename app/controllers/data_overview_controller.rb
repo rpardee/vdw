d8a6e268b7ec99ba317829bc65cb4f778acca8a4
Roy Pardee <pardee.r@ghc.org>
2010-02-22 08:56:01 -0800

    Added stoplighting to overview.

--- app/controllers/data_overview_controller.rb
+++ app/controllers/data_overview_controller.rb
@@ -2,7 +2,8 @@ class DataOverviewController < ApplicationController
   before_filter :require_user
 
   def index
-    @sites = Site.find(:all, :order => "standard_code")
+    @implementing_sites = Site.find(:all, :order => "standard_code", :conditions => "id in (select site_id from dataset_implementations)")
+    @non_implementing_sites = Site.find(:all, :order => "standard_code", :conditions => "id NOT in (select site_id from dataset_implementations)")
     @datasets = Dataset.find(:all, :order => "sort_order")
   end
 
--- app/helpers/data_overview_helper.rb
+++ app/helpers/data_overview_helper.rb
@@ -1,11 +1,16 @@
 module DataOverviewHelper
   def cell_contents(d, s)
+    ret = ""
+    cls = "green-cell"
     if s.datasets.include?(d) then 
       # TODO: Change title to show number of current issues and caveats.
       di = s.dataset_implementations.find_by_dataset_id(d)
-      link_to "Strt: #{di.start_date.to_s(:month_and_year)} <br />Upd: #{di.update_frequency}", di, {:title => "#{di.open_issues.length} open issues; #{di.caveats.length} caveats."}
+      ret = link_to "Strt: #{di.start_date.to_s(:month_and_year)} <br />Upd: #{di.update_frequency}", di, {:title => "#{di.open_issues.length} open issues; #{di.caveats.length} caveats."}
+      cls = di.open_issues.length > 0 ? 'yellow-cell' : 'green-cell'
     else
-      link_to "NI", {:action => "new", :controller => "dataset_implementations", :site_id => s.id, :dataset_id => d.id}, {:title => 'Click to create'}
+      ret = link_to "NI", {:action => "new", :controller => "dataset_implementations", :site_id => s.id, :dataset_id => d.id}, {:title => 'Click to create'}
+      cls = 'red-cell'
     end
+    "<td class = '#{cls}'>#{ret}</td>"
   end
 end
