cb635c7d95595fbc7cefcdec9aa36a146e0af7f0
Roy Pardee <pardee.r@ghc.org>
2010-02-22 09:03:33 -0800

    Stoplight overview tweaks.

--- app/helpers/data_overview_helper.rb
+++ app/helpers/data_overview_helper.rb
@@ -8,7 +8,7 @@ module DataOverviewHelper
       ret = link_to "Strt: #{di.start_date.to_s(:month_and_year)} <br />Upd: #{di.update_frequency}", di, {:title => "#{di.open_issues.length} open issues; #{di.caveats.length} caveats."}
       cls = di.open_issues.length > 0 ? 'yellow-cell' : 'green-cell'
     else
-      ret = link_to "NI", {:action => "new", :controller => "dataset_implementations", :site_id => s.id, :dataset_id => d.id}, {:title => 'Click to create'}
+      ret = link_to "Not Impl.", {:action => "new", :controller => "dataset_implementations", :site_id => s.id, :dataset_id => d.id}, {:title => 'Click to create'}
       cls = 'red-cell'
     end
     "<td class = '#{cls}'>#{ret}</td>"
